fae3841e3109d6b7358acb8cead82ff253cf0b72
Josef Vágner <48518988+josefvagner@users.noreply.github.com>
2024-02-21 08:42:47 +0100

    add uart

---
+++ Programs/serialCommunication/uartEcho/main.py
@@ -0,0 +1,12 @@
+from machine import Pin, UART
+import time
+
+uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+uart.init(bits=8, parity=0, stop=1)
+
+while True:
+    if uart.any():
+        data = uart.read()
+        if data == b"t":
+            uart.write("r")
+    time.sleep(1)
---
+++ Programs/serialCommunication/uartEcho/uartDev1.py
@@ -0,0 +1,22 @@
+from machine import Pin, UART
+import time
+
+uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+uart.init(bits=8, parity=0, stop=1)
+
+while True:
+    print("sending...")
+    uart.write("t")
+    confirmed = False
+    print("waiting for response ")
+    while not confirmed:
+        print(".", end="")
+        if uart.any():
+            data = uart.read()
+            if data == b"r":
+                print("msg confirmed")
+                confirmed = True
+            else:
+                print("wrong data received")
+        time.sleep(0.2)
+    time.sleep(1)
